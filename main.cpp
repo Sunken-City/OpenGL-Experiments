c7a82ef05db9b188adf74e8fdb947fe44362f606
picoriley <acloudy@mail.smu.edu>
2015-03-14 19:54:23 -0500

    I have discovered that this is NOT a cube. Yet.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <GLFW/glfw3.h> // Include GLFW, the Windowing Tool Kit we're using.
 
 // Include GLM core features
-#include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 
 #include <stdio.h>
 #include <assert.h>
@@ -72,36 +74,25 @@ int main()
 	Program shaderProgram = Program(vs, fs);
 
 	//Movement tutorial stuff
-	float matrix[] = {
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.5f, 0.0f, 0.0f, 1.0f,
-	};
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Camera matrix
+	glm::mat4 View = glm::lookAt(
+		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+		glm::vec3(0, 0, 0), // and looks at the origin
+		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);  // Changes for each model !
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
-	int matrix_location = shaderProgram.getUniform("matrix");
+	int matrix_location = shaderProgram.getUniform("MVP");
 	shaderProgram.use();
-	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, matrix);
+	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, &MVP[0][0]);
 
-	float speed = 1.0f;
-	float last_position = 0.0f;
 	while (!glfwWindowShouldClose(window))
 	{
-		//Add a timer for animation
-		static double previous_seconds = glfwGetTime();
-		double current_seconds = glfwGetTime();
-		double elapsed_seconds = current_seconds - previous_seconds;
-		previous_seconds = current_seconds;
-
-		if (fabs(last_position) > 1.0f)
-		{
-			speed = -speed;
-		}
-		matrix[12] = elapsed_seconds * speed + last_position;
-		last_position = matrix[12];
-		shaderProgram.use();
-		glUniformMatrix4fv(matrix_location, 1, GL_FALSE, matrix);
-
 		//Clear the drawing screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
