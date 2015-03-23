1ee595ca76ad423d115b8bab8bbe97ff6c379da7
picoriley <acloudy@mail.smu.edu>
2015-03-23 17:56:40 -0500

    Implemented a camera to view the rest of the model

--- main.cpp
+++ main.cpp
@@ -16,6 +16,23 @@
 
 using namespace glm;
 
+void updateMVP(glm::vec3 camPos, float camYaw, int MVPLocation)
+{
+	glm::mat4 Translation = glm::translate(glm::mat4(), -camPos);
+	glm::mat4 Rotation = glm::rotate(glm::mat4(), -camYaw, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 View = Rotation * Translation;
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);  // Changes for each model !
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, &MVP[0][0]);
+}
+
 int main()
 {
 	assert(glLog::restart_gl_log());
@@ -62,26 +79,89 @@ int main()
 
 	Program shaderProgram = Program(vs, fs);
 
-	//Movement tutorial stuff
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	glm::mat4 View = glm::lookAt(
-		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-		);
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4(1.0f);  // Changes for each model !
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+	//Camera stuff.
+	float cam_speed = 1.0f; //1 unit per second
+	float cam_yaw_speed = 5.0f; //5 degrees per second
+	glm::vec3 camPos = vec3(0.0f, 0.0f, 10.0f);
+	float camYaw = 0.0f; //Y rotation in degrees.
 
-	int matrix_location = shaderProgram.getUniform("MVP");
+	int MVPLocation = shaderProgram.getUniform("MVP");
 	shaderProgram.use();
-	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, &MVP[0][0]);
+
+	updateMVP(camPos, camYaw, MVPLocation);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
+		//Update our Timer
+		static double previousSeconds = glfwGetTime();
+		double currentSeconds = glfwGetTime();
+		double elapsedSeconds = currentSeconds - previousSeconds;
+		previousSeconds = currentSeconds;
+
+		//Camera Control
+		bool camMoved = false;
+		
+		//If holding left shift, "Run"
+		if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT))
+		{
+			cam_speed = 2.0f;
+			cam_yaw_speed = 10.0f;
+		}
+		else
+		{
+			cam_speed = 1.0f;
+			cam_yaw_speed = 5.0f;
+		}
+		if (glfwGetKey(window, GLFW_KEY_A))
+		{
+			camPos.x -= cam_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (glfwGetKey(window, GLFW_KEY_D))
+		{
+			camPos.x += cam_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (glfwGetKey(window, GLFW_KEY_SPACE))
+		{
+			camPos.y += cam_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL))
+		{
+			camPos.y -= cam_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (glfwGetKey(window, GLFW_KEY_W))
+		{
+			camPos.z -= cam_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (glfwGetKey(window, GLFW_KEY_S))
+		{
+			camPos.z += cam_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (glfwGetKey(window, GLFW_KEY_Q))
+		{
+			camYaw += cam_yaw_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (glfwGetKey(window, GLFW_KEY_E))
+		{
+			camYaw -= cam_yaw_speed * elapsedSeconds;
+			camMoved = true;
+		}
+		if (camMoved)
+		{
+			updateMVP(camPos, camYaw, MVPLocation);
+		}
+
+
+
+
+
 		//Clear the drawing screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
